14efd2d1b04c25ecd7c1ac374cf5a98e461f4106
botoysigma <sigmawooddog@gmail.com>
2025-11-09 00:08:58 +0530

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center text-gray-500 dark:text-gray-400 p-4 text-sm mt-8">
-      <p>Version 1.0.0</p>
-      <p>Made by Lishith</p>
-    </footer>
-  );
-}
